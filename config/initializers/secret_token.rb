7ad0daa9c753d3908dcfa8bb3345c4c79de8375d
Apprentice <mail@danbender.net>
2013-11-18 15:09:54 -0800

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovesAppApi::Application.config.secret_token = 'c65e35122e4d226ba763a4759743794b2988bebfb3671018362ad99fa4b546e82eaa31fa804d1ecc1a4ecdc13b13a27d16c5e33ab374c6ad603873156b9c51ae'
